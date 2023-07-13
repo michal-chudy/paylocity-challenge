ff07352f41306a6dfe53951e2baf3cac19cbf2c5
Michal Chudy <michal@blinderhq.com>
2023-07-13 19:14:18 +0200

    Updating the next payroll date to August 23

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,6 @@ insert into employee_salary(employee_id, amount, annual_payroll_count) values (1
 insert into employee_salary(employee_id, amount, annual_payroll_count) values (2, 200000, 26);  -- Jane
 insert into employee_salary(employee_id, amount, annual_payroll_count) values (3, 200000, 26);  -- Anna
 
-insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-07-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 1, 1); -- John
-insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-07-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 2, 2); -- Jane
-insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-07-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 3, 3); -- Anna
+insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-08-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 1, 1); -- John
+insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-08-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 2, 2); -- Jane
+insert into payroll(payroll_date, payroll_status, gross_amount, benefit_deductions, net_amount, employee_id, employee_salary_id) values ('2023-08-06 13:00:00.000', 'DRAFT', 200000, 0, 200000, 3, 3); -- Anna
